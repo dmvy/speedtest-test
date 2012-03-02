44e377de9413c7de3694bd62f3dfc40137b9f9af
Ilya Shchekalev <git@dmvy.ru>
2012-03-02 17:17:02 +0600

    Source IP can be changed.

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 cd ~/speedtest
 
+IP=127.0.0.1
+
 HOSTS=( $(cat hosts))
 HOSTS_cnt=`cat hosts|wc -l`
 for ((i=0;  i<$HOSTS_cnt*5-1; i+=5)); do
@@ -12,9 +14,9 @@ for ((i=0;  i<$HOSTS_cnt*5-1; i+=5)); do
 		mkdir $HOST
 		touch $HOST/speed.log
 	fi
-	/usr/bin/wget --progress=dot:mega -o $HOST/wget.log -O /dev/null http://$HOST/$FILE
+	/usr/bin/wget --bind-address=$IP --progress=dot:mega -o $HOST/wget.log -O /dev/null http://$HOST/$FILE
 	grep saved $HOST/wget.log >> $HOST/speed.log
 	DT=`date +%Y-%m-%d_%H-%M`
-	/usr/bin/mtr -wrc100 $HOST > $HOST/$DT.mtr &
+	/usr/bin/mtr --address $IP -wrc100 $HOST > $HOST/$DT.mtr &
 done;
 ./analyse.sh
