b61345bdb649f6b28b1c4db3e7071f78a01e8b37
Ilya Shchekalev <git@dmvy.ru>
2012-04-02 18:58:04 +0600

    В вывод работы добавлены город и провайдер

--- test.sh
+++ test.sh
@@ -7,9 +7,11 @@ IP=127.0.0.1
 HOSTS=( $(cat hosts))
 HOSTS_cnt=`cat hosts|wc -l`
 for ((i=0;  i<$HOSTS_cnt*4-1; i+=4)); do
+	CITY=${HOSTS[i]}
+	ISP=${HOSTS[i+1]}
 	HOST=${HOSTS[i+2]}
 	FILE=${HOSTS[i+3]}
-	echo $HOST $FILE
+	echo $CITY\t$ISP\t$HOST
 	if [ ! -d $HOST ]; then
 		mkdir $HOST
 		touch $HOST/speed.log
